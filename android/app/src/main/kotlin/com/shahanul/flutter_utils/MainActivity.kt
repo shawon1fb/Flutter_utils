38fc6eb104b38f78909fa6d7118b2658bc4d7f52
shawon1fb <shawon0187@gmail.com>
2020-08-18 23:34:15 +0600

    initial

---
+++ android/app/src/main/kotlin/com/shahanul/flutter_utils/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.shahanul.flutter_utils
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import 'debug/flutter_json_viewer.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Utils',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      //home: MyHomePage(),
+      home: FlutterJsonViewerPage(),
+    );
+  }
+}
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
